6cb60c444349bd6794e9173ec7a8461ae809c4ff
Rafael Porreca <rafael.porreca@gmail.com>
2021-03-22 17:06:01 -0300

    Projeto criado

---
+++ src/main/java/br/com/rafaelporreca/dsbootcamp/DsbootcampApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafaelporreca.dsbootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsbootcampApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsbootcampApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/rafaelporreca/dsbootcamp/DsbootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafaelporreca.dsbootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsbootcampApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
