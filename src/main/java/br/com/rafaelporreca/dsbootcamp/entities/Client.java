65c8ee8b47ab615523d380ae42504b63d6fbd879
Rafael Porreca <rafael.porreca@gmail.com>
2021-03-26 14:44:23 -0300

    Client persistence config

--- src/main/java/br/com/rafaelporreca/dsbootcamp/entities/Client.java
+++ src/main/java/br/com/rafaelporreca/dsbootcamp/entities/Client.java
@@ -1,7 +1,6 @@
 package br.com.rafaelporreca.dsbootcamp.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
@@ -10,6 +9,10 @@ import java.util.Objects;
 @Table(name = "tb_client")
 public class Client implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String cpf;
